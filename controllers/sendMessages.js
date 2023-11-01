d0aa10700b74f3df459adc790052c29b88bebd35
AdityaRokade17 <rokadeaditya634@gmail.com>
2023-11-01 15:43:06 +0530

    add

--- controllers/sendMessages.js
+++ controllers/sendMessages.js
@@ -1,38 +1,35 @@
 const User = require("../models/User");
 
-exports.sendMessage = async ( req, res) => {
-    try{
-        
+exports.sendMessage = async (req, res) => {
+    try {
         console.log(req.body);
-        
-        const { name , email , comment} = req.body;
+
+        const { name, email, comment } = req.body;
         console.log(req.body);
         if (!name || !email || !comment) {
             console.log("not all fields...");
             return res.status(400).json({
-              status: 400,
-              message: "Please fill all fields",
+                status: 400,
+                message: "Please fill all fields",
             });
         }
 
         const user = await User.create({
             name,
             email,
-            comment
+            comment,
         });
 
-        return res.status(200).json({
+        return res.status(201).json({
             status: 201,
-            message : "message Send Succesfully",
-            data: user
+            message: "Message sent successfully",
+            data: user,
         });
-
-
-    }catch (error) {
-        console.log("error", error);
+    } catch (error) {
+        console.error("error", error);
         return res.status(500).json({
-          status: 500,
-          message: error.comment,
+            status: 500,
+            message: error.message,
         });
     }
-}
+};
